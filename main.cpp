97e7db04619d76221c60fca2b75b52c85e4fc812
longyf <longyf@pku.edu.cn>
2018-08-09 20:50:39 +0800

    Initial comment
    match.h is an example.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "match.h"
+using namespace std;
+void test1() {
+	cout<<"Test1."<<endl;
+	char str[6] = {'a', 'b', 'c', 'c', 'c', '\0'};
+	char pattern[5] = {'a', '.', 'c', '*', '\0'};
+	if (match(str, pattern)) {
+		cout<<"Matched."<<endl;
+	}
+	else {
+		cout<<"Not matched."<<endl;
+	}
+}
+
+void test2() {
+	cout<<"Test2."<<endl;
+	char str[6] = {'a', 'b', 'c', 'c', 'c', '\0'};
+	char pattern[5] = {'a', '.', '*', '*', '\0'};
+	if (match(str, pattern)) {
+		cout<<"Matched."<<endl;
+	}
+	else {
+		cout<<"Not matched."<<endl;
+	}
+}
+int main() {
+	test1();
+	test2();
+	return 0;
+}
+
+
